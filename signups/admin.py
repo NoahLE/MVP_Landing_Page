571b17f1ebe66f67e3cc3202d3b4d8e90fbdeee2
DigitalMockingbird <noah@digitalmockingbird.com>
2014-07-11 16:36:07 -0700

    End video 4

--- signups/admin.py
+++ signups/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import SignUp
+
+class SignUpAdmin(admin.ModelAdmin):
+    class Meta:
+        model = SignUp
+
+# makes signups app / model renderable in admin panel
+admin.site.register(SignUp, SignUpAdmin)
--- signups/models.py
+++ signups/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+# for support of foreign characters
+from django.utils.encoding import smart_unicode
 
 # Create your models here.
+# signup singular instance (below) -> signups for all instances (app)
+class SignUp(models.Model):
+    # null = database ok blank
+    # blank = form ok blank when submitted
+    # charfield = max_length always required
+    first_name = models.CharField(max_length=120, null=True, blank=True)
+    last_name = models.CharField(max_length=120, null=True, blank=True)
+    email = models.EmailField(null=False, blank=False)
+    # auto_now_true = timestamp when created
+    # auto_now = timestamp for changes
+    timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
+    updated = models.DateTimeField(auto_now_add=False, auto_now=True)
+
+    def __unicode__(self):
+        return smart_unicode(self.email)
