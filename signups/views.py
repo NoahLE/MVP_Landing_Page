1741d24a4fd68828b79943a09d9afb419880b1db
DigitalMockingbird <noah@digitalmockingbird.com>
2014-07-12 09:45:47 -0700

    End video 11

--- signups/views.py
+++ signups/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, render_to_response, RequestContext
+from django.contrib import messages
 
 # Create your views here.
 from .forms import SignUpForm
@@ -9,6 +10,7 @@ def home(request):
     if form.is_valid():
         save_it = form.save(commit=False)
         save_it.save()
+        messages.success(request, 'Thank you for joining!')
 
     return render_to_response("signup.html",
                               locals(),
